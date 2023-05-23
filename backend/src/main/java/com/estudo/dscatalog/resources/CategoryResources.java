aa04b25c2d6d1cfa3d8a4f1f798e0410c96a3402
Clara <clara.gabrielle@youxgroup.com.br>
2023-05-23 13:57:38 -0300

    fazendo busca por id da categoria e criando mensagem personalizada de erro

--- backend/src/main/java/com/estudo/dscatalog/resources/CategoryResources.java
+++ backend/src/main/java/com/estudo/dscatalog/resources/CategoryResources.java
@@ -6,6 +6,7 @@ import com.estudo.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class CategoryResources {
     public ResponseEntity<List<CategoryDTO>> findAll() {
         List<CategoryDTO> categories = categoryService.findAll();
         return ResponseEntity.ok().body(categories);
+    }
+    @GetMapping(value="/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO categoryDto = categoryService.findById(id);
+        return ResponseEntity.ok().body(categoryDto);
     }
 }
--- backend/src/main/java/com/estudo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/estudo/dscatalog/services/CategoryService.java
@@ -3,12 +3,14 @@ package com.estudo.dscatalog.services;
 import com.estudo.dscatalog.dto.CategoryDTO;
 import com.estudo.dscatalog.entities.Category;
 import com.estudo.dscatalog.repositories.CategoryRepository;
+import com.estudo.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 // Registra como um componente que participa do sistema de injeção de dependencia automatizado do spring.
@@ -22,5 +24,15 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> categories = categoryRepository.findAll();
         return categories.stream().map(cat -> new CategoryDTO(cat)).collect(Collectors.toList());
+    };
+
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        //Optional: para nunca ter valor nulo. Mas dentro dele, pode ou não ter categorias
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+
     };
 }
